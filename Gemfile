04fc0943a54f602e9a7c89b5a41541ba6ae9067b
vishnu <vishnukp.me@gmail.com>
2024-09-06 05:12:17 +0530

    Added has_secure_password and validations to User model

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ gem "shakapacker", "~> 6.6.0"
 gem "react-rails", "~> 2.7.1"
 
 gem "pry-byebug"
+
+gem "bcrypt", "~> 3.1.13"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_31_201837) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_05_232653) do
   create_table "tasks", force: :cascade do |t|
     t.text "title", null: false
     t.datetime "created_at", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_31_201837) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users", column: "assigned_user_id"
