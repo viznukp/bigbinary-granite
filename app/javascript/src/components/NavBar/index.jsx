1ec9447bcd52df34a4ab90cbe6142fcd29c326c8
vishnu <vishnukp.me@gmail.com>
2024-09-10 13:29:04 +0530

    Added ability to logout

--- app/javascript/src/components/NavBar/index.jsx
+++ app/javascript/src/components/NavBar/index.jsx
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import classnames from "classnames";
 import { Link, useLocation } from "react-router-dom";
+import { resetAuthTokens } from "src/apis/axios";
 
-import { getFromLocalStorage } from "utils/storage";
+import authApi from "apis/auth";
+import { getFromLocalStorage, setToLocalStorage } from "utils/storage";
 
 import GraniteLogo from "./GraniteLogo";
 
 const NavBar = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const menuRef = useRef();
   const userName = getFromLocalStorage("authUserName");
   const location = useLocation();
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await authApi.logout();
+      setToLocalStorage({
+        authToken: null,
+        email: null,
+        userId: null,
+        userName: null,
+      });
+      resetAuthTokens();
+      window.location.href = "/";
+    } catch (error) {
+      logger.error(error);
+    }
+  };
 
   return (
     <header className="bg-primary-white sticky top-0 z-20 w-full border-b border-gray-200 transition-all duration-500">
       <div className="mx-auto max-w-6xl px-6">
         <div className="flex h-16 items-center justify-between">
           <div className="w-max flex-shrink-0">
-            <Link className="h-full w-auto" to="/dashboard">
+            <Link className="h-full w-auto" to="/">
               <GraniteLogo className="h-8 w-auto" />
             </Link>
           </div>
@@ -24,7 +62,7 @@ const NavBar = () => {
             <Link
               to="/"
               className={classnames("text-sm font-medium text-gray-800", {
-                "text-indigo-600": location.pathname === "/dashboard",
+                "text-indigo-600": location.pathname === "/",
               })}
             >
               Todos
@@ -35,9 +73,25 @@ const NavBar = () => {
             >
               Add new task
             </Link>
-            <Link className="flex items-center gap-x-1 rounded-md bg-gray-200 px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-300 focus:shadow">
-              <span className="block">{userName}</span>
-            </Link>
+            <div className="relative" ref={menuRef}>
+              <Link
+                className="flex items-center gap-x-1 rounded-md bg-gray-200 px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-300 focus:shadow"
+                onClick={toggleMenu}
+              >
+                <span className="block">{userName}</span>
+                <i className="ri-arrow-down-s-line" />
+              </Link>
+              {isMenuVisible && (
+                <div className="absolute right-0 z-20 mt-2 w-48 rounded-md border border-gray-300 bg-white py-1 shadow-xl">
+                  <Link
+                    className="block cursor-pointer px-3 py-1.5 text-sm text-gray-800 hover:bg-gray-100"
+                    onClick={handleLogout}
+                  >
+                    Log out
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
