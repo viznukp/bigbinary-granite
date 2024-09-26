d401aed3938ab12d22912230c5890ca279be330a
vishnu <vishnukp.me@gmail.com>
2024-09-26 20:22:41 +0530

    Fixed order of items in Active Record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :comments, dependent: :destroy
-  has_secure_password
-  has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
@@ -22,6 +20,9 @@ class User < ApplicationRecord
 
   before_save :to_lowercase
   before_destroy :assign_tasks_to_task_owners
+
+  has_secure_password
+  has_secure_token :authentication_token
 
   private
 
